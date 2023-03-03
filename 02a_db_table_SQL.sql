957c4e192c3c98b9613ee19c8c362616c86dd34b
przygoda <schmidt.multimedia@googlemail.com>
2023-03-03 11:19:33 +0000

    Tabelle angelegt

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -4,8 +4,6 @@
 #  Kommentar 2
 -- Kommentar 3
 
-/* Datenbanken auf Server anzeigen */
-#SHOW DATABASES;
 
 /* DB boo löschen, falls vorhanden*/
 DROP DATABASE IF EXISTS boo;
@@ -13,4 +11,21 @@ DROP DATABASE IF EXISTS boo;
 /* DB boo anlegen, falls noch nicht vorhanden*/
 CREATE DATABASE IF NOT EXISTS boo;
 
+/* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
+
+/* DB auswählen */
+USE boo;
+
+DROP TABLE IF EXISTS coworkers;
+CREATE TABLE IF NOT EXISTS coworkers
+(
+    firstName VARCHAR(20),
+    location VARCHAR(20),
+    age INT,
+    computer VARCHAR(20)   
+);
+
+SHOW TABLES;
+
+DESCRIBE coworkers;
